a0a6e81caeb800cca838b1637a9d30ed14316b25
wptb <stevo.chromik@gmail.com>
2016-03-05 13:30:16 +0100

    docker+openvpn

---
+++ docker/recipes/install.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: docker
+# Recipe:: default
+#
+case node[:platform]
+when 'ubuntu','debian'
+  package 'docker.io' do
+    action :install
+  end
+when 'centos','redhat','fedora','amazon'
+  package 'docker' do
+    action :install
+  end
+end
+
+service 'docker' do
+  action :start
+end
